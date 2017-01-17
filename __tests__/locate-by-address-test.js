78d0b2823bd3ca12784f178392317c44874486e2
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-01-17 11:34:17 -0500

    remove a test

--- __tests__/locate-by-address-test.js
+++ __tests__/locate-by-address-test.js
@@ -9,34 +9,34 @@ const SiteMonitoring = require('../index');
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // 10 second timeout
 
 describe('Lookup server location by address', () => {
-    // it('Create instance', () => {
-    //     let siteMonitoring = new SiteMonitoring(); 
-    //     return expect(siteMonitoring).toBeInstanceOf(SiteMonitoring);
-    // });
+    it('Create instance', () => {
+        let siteMonitoring = new SiteMonitoring(); 
+        return expect(siteMonitoring).toBeInstanceOf(SiteMonitoring);
+    });
 
     /**
      * get running server ip 
      * call locateByAddress to get its location information
      */
-    it('finds the server running the code', () => {
-        let debug = true;
-        let siteMonitoring = new SiteMonitoring(debug); 
+    // it('finds the server running the code', () => {
+    //     let debug = true;
+    //     let siteMonitoring = new SiteMonitoring(debug); 
 
-        // return publicIp.v4().then(ip => siteMonitoring.locateByAddress(ip))
-        let ip = '172.217.4.68';
-        return siteMonitoring.locateByAddress(ip)
-        .then(result => {
-            // result = { origin, destination }
-            expect(result).toBeDefined();
-            // origin = { region_code, latitude, longitude }
-            expect(result.origin).toBeDefined();
-            // destination = [ { region_code, latitude, longitude } ]
-            return expect(result.destination).toBeInstanceOf(Array);
+    //     // return publicIp.v4().then(ip => siteMonitoring.locateByAddress(ip))
+    //     let ip = '172.217.4.68';
+    //     return siteMonitoring.locateByAddress(ip)
+    //     .then(result => {
+    //         // result = { origin, destination }
+    //         expect(result).toBeDefined();
+    //         // origin = { region_code, latitude, longitude }
+    //         expect(result.origin).toBeDefined();
+    //         // destination = [ { region_code, latitude, longitude } ]
+    //         return expect(result.destination).toBeInstanceOf(Array);
 
-        })
-        .catch(e => {
-            expect(e).toBeNull();
-            return expect(e).toBeInstanceOf(Error);           
-        });
-    });
+    //     })
+    //     .catch(e => {
+    //         expect(e).toBeNull();
+    //         return expect(e).toBeInstanceOf(Error);           
+    //     });
+    // });
 });
