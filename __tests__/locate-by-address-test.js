21d9ecb798b9773c436e1eb1b134382296f81cdd
Marcellin Nshimiyimana <mars@fusemachines.com>
2017-01-17 11:58:43 -0500

    test self ip

--- __tests__/locate-by-address-test.js
+++ __tests__/locate-by-address-test.js
@@ -5,7 +5,7 @@
  */
 
 const SiteMonitoring = require('../index');
-// const publicIp = require('public-ip');
+const publicIp = require('public-ip');
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000; // 10 second timeout
 
 describe('Lookup server location by address', () => {
@@ -18,25 +18,25 @@ describe('Lookup server location by address', () => {
      * get running server ip 
      * call locateByAddress to get its location information
      */
-    // it('finds the server running the code', () => {
-    //     let debug = true;
-    //     let siteMonitoring = new SiteMonitoring(debug); 
+    it('finds the server running the code', () => {
+        let debug = true;
+        let siteMonitoring = new SiteMonitoring(debug); 
 
-    //     // return publicIp.v4().then(ip => siteMonitoring.locateByAddress(ip))
-    //     let ip = '172.217.4.68';
-    //     return siteMonitoring.locateByAddress(ip)
-    //     .then(result => {
-    //         // result = { origin, destination }
-    //         expect(result).toBeDefined();
-    //         // origin = { region_code, latitude, longitude }
-    //         expect(result.origin).toBeDefined();
-    //         // destination = [ { region_code, latitude, longitude } ]
-    //         return expect(result.destination).toBeInstanceOf(Array);
+        // return publicIp.v4().then(ip => siteMonitoring.locateByAddress(ip))
+        // return siteMonitoring.locateByAddress(ip)
+        return publicIp.v4()
+        .then(result => {
+            // result = { origin, destination }
+            expect(result).toBeDefined();
+            // origin = { region_code, latitude, longitude }
+            expect(result.origin).toBeDefined();
+            // destination = [ { region_code, latitude, longitude } ]
+            return expect(result.destination).toBeInstanceOf(Array);
 
-    //     })
-    //     .catch(e => {
-    //         expect(e).toBeNull();
-    //         return expect(e).toBeInstanceOf(Error);           
-    //     });
-    // });
+        })
+        .catch(e => {
+            expect(e).toBeNull();
+            return expect(e).toBeInstanceOf(Error);           
+        });
+    });
 });
